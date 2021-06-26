3d17552ac47bf5bcc7640b0ccb2b654eaf97044f
CaioCaminha <caminhacaiopro@gmail.com>
2021-06-26 16:22:40 -0300

    API Gateway

---
+++ src/test/java/caio/caminha/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package caio.caminha.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
